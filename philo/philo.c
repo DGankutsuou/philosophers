fd24978abbdc5afae5474fab1257fd2d2033c62b
DGankutsuou <DGankutsuou@github.com>
2025-06-01 21:31:56 +0100

    started the real work

--- philo/philo.c
+++ philo/philo.c
@@ -3,10 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aabouriz <aabouriz@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: blessed <blessed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 09:05:31 by aabouriz          #+#    #+#             */
-/*   Updated: 2025/06/01 09:05:32 by aabouriz         ###   ########.fr       */
+/*   Updated: 2025/06/01 21:30:03 by blessed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+void	*life_cycle(void *data)
+{
+	t_args	*args;
+
+	args = (t_args *)data;
+	if (args->number_of_philos % 2 == 0)
+		even_cycle(args);
+	else
+		even_cycle(args);
+}
